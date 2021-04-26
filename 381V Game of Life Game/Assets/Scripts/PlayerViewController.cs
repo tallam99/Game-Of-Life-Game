b5b7fac80af443af393bc3ace9b32365c4319138
tallam99 <tarekyallam99@gmail.com>
2021-04-26 13:40:44 -0500

    Lock cursor to center of screen and hide it

--- 381V Game of Life Game/Assets/Scripts/PlayerViewController.cs
+++ 381V Game of Life Game/Assets/Scripts/PlayerViewController.cs
@@ -43,7 +43,7 @@ public class PlayerViewController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
